95096d0815789c6aee15410b7a216b4fa70260b2
IAmBiny <anhtruong100703@gmail.com>
2024-11-19 17:30:41 +0700

    fix 'session.id' -> 'session.user.id'

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = async () => {
                             }}>
                                 <button type="submit">Logout</button>
                             </form>
-                            <Link href={`/user/${session?.id}`}>
+                            <Link href={`/user/${session.user.id}`}>
                                 <span>{session.user.name}</span>
                             </Link>
                         </>
