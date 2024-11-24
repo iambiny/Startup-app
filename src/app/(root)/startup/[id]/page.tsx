7b8b17a90f5c4d5ae35351a2c356e0e89ed29da5
IAmBiny <anhtruong100703@gmail.com>
2024-11-24 19:17:00 +0700

    Hoàn thiện trang 'Detail Startup', thêm Parallel Fetching, sửa Toast

--- src/app/(root)/startup/[id]/page.tsx
+++ src/app/(root)/startup/[id]/page.tsx
@@ -6,18 +6,28 @@ import { notFound } from "next/navigation";
 import { Suspense } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, { StartupCardType } from "@/components/StartupCard";
 
 export const experimental_ppr = true;
 // export const revalidate = 60;       // enable ISG for hole page
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
     const id = (await params).id;
-    const res = await fetch(`http://localhost:8000/api/startups/${id}`, {
-        next: {
-            revalidate: 60      // enable ISG for only fetch data
-        }
-    });
-    const post = await res.json();
+    // Parallel Fetching
+    const [res1, res2] = await Promise.all([
+        fetch(`http://localhost:8000/api/startups/${id}`, {
+            next: {
+                revalidate: 60      // enable ISG for only fetch data
+            }
+        }),
+        fetch(`http://localhost:8000/api/startups/getRelatedPosts/${'Startup of the Week'}`)
+
+    ])
+    const [post, playlist] = await Promise.all([
+        res1.json(),
+        res2.json()
+    ])
+
     if (!post) return notFound();
 
     return (
@@ -59,6 +69,17 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
                     </div>
                 </div>
                 <hr className="divider" />
+
+                {playlist.length > 0 && (
+                    <div className="max-w-4xl mx-auto">
+                        <p className="text-30-semibold">Startup of the Week</p>
+                        <ul className="mt-7 card_grid-sm">
+                            {playlist.map((post: StartupCardType, idx: number) => (
+                                <StartupCard key={idx} post={post} />
+                            ))}
+                        </ul>
+                    </div>
+                )}
 
                 {/* Dynamic Rendering */}
                 <Suspense fallback={<Skeleton className="view_skeleton" />}>
